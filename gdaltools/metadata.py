fa1cce7b37f299ae19ca45dba037b1af3dda5aad
dispiste <cesar.izq@gmail.com>
2016-11-17 17:02:34 +0100

    Upgrade version

--- gdaltools/metadata.py
+++ gdaltools/metadata.py
@@ -6,9 +6,9 @@ Information describing the project.
 
 # The package name, which is also the "UNIX name" for the project.
 package = 'pygdaltools'
-project = "pygdaltools"
+project = 'pygdaltools'
 project_no_spaces = project.replace(' ', '')
-version = '0.1'
+version = '0.2'
 description = """Python wrapper for Gdal/OGR command line tools"""
 authors = ['Cesar Martinez Izquierdo - SCOLAB']
 authors_string = ', '.join(authors)
