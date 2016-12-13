73f3c743139927a049c5e733f98e1fcb8125721a
dispiste <cesar.izq@gmail.com>
2016-12-13 17:34:32 +0100

    Update release version

--- gdaltools/metadata.py
+++ gdaltools/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'pygdaltools'
 project = 'pygdaltools'
 project_no_spaces = project.replace(' ', '')
-version = '0.2'
+version = '0.3'
 description = """Python wrapper for Gdal/OGR command line tools"""
 authors = ['Cesar Martinez Izquierdo - SCOLAB']
 authors_string = ', '.join(authors)
