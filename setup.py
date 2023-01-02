da90dc15ed13a0c9cdf95ea48474780152cd0cc0
Cesar Martinez Izquierdo <cesar.izq@gmail.com>
2023-01-02 17:51:35 +0100

    Update build dependencies and declare python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -250,6 +250,8 @@ setup_dict = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: GIS',
         'Topic :: Utilities',
         'Topic :: Database',
